326b135b69c4df16f1d0fa2f3b3893929cf28a41
prateeksethi002 <prateeksethi2989@gmail.com>
2015-12-20 08:09:29 -0800

    Dynamically adding rows to the result table

--- app/src/main/java/com/nptech/schoolexamresults/showResult.java
+++ app/src/main/java/com/nptech/schoolexamresults/showResult.java
@@ -6,6 +6,9 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class showResult extends AppCompatActivity {
 
@@ -15,6 +18,36 @@ public class showResult extends AppCompatActivity {
         setContentView(R.layout.activity_show_result);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        TableLayout result = (TableLayout)findViewById(R.id.resultTable);
+        TableRow dataRow = new TableRow(this);
+
+        TextView examName = new TextView(this);
+        examName.setText("FirstSemester");
+        examName.setPadding(10, 10, 10, 10);
+        dataRow.addView(examName,0);
+
+        TextView subjectName = new TextView(this);
+        subjectName.setText("Maths");
+        subjectName.setPadding(10, 10, 10, 10);
+        dataRow.addView(subjectName, 1);
+
+        TextView maxMarks = new TextView(this);
+        maxMarks.setText("100");
+        maxMarks.setPadding(10, 10, 10, 10);
+        dataRow.addView(maxMarks, 2);
+
+        TextView passingMarks = new TextView(this);
+        passingMarks.setText("50");
+        passingMarks.setPadding(10, 10, 10, 10);
+        dataRow.addView(passingMarks, 3);
+
+        TextView marksObtained = new TextView(this);
+        marksObtained.setText("98");
+        marksObtained.setPadding(10,10,10,10);
+        dataRow.addView(marksObtained,4);
+
+        result.addView(dataRow);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
